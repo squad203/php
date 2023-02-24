56bb0b7bb42966c9084764543f74194dd335a3d8
squad203 <jakasaniyasmit@gmail.com>
2023-02-24 07:39:23 +0530

    dd

--- 22-02-23/02variabal.php
+++ 22-02-23/02variabal.php
@@ -1,8 +1,17 @@
 <?php
-    $a =20001000;
-    $b =999999;
-    $s =999;
-    $c =$a + $b - $s;
-    echo $c;
+//     $a =20001000;
+//     $b =999999;
+//     $s =999;
+//     $c =$a + $b - $s;
+//     echo $c;
 
+// $a = 10  ;
+// $c = 000;
+// $s = $a/$c;
+      // echo $s;
+
+$movie = "";
+echo "I'm a fickle person, my favorite movie used to be $movie.";
+$movie = "hero  ";
+echo "\nBut now my favorite is $movie.";
 ?>
