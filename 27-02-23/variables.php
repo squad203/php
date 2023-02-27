2f217d9f2731033858d6e75e6af1c8ad958c6089
gurukulsk <gurukulgetpass@gmail.com>
2023-02-27 07:49:35 +0530

    New php file

--- 27-02-23/variables.php
+++ 27-02-23/variables.php
@@ -1,5 +1,10 @@
 <?php
-
-    echo "This is phph leactur in millenium hall";
+    echo "<center>";
+    
+    echo "<br>This is a best php file<br>";
+    echo "<br>This is php  leactur in millenium hall<br>";
+    
+    
+    echo  "</center>";
 
 ?>
