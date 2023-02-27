9d468210258d399c148ab769c4feea049e1ae3b6
squad203 <jakasaniyasmit@gmail.com>
2023-02-27 08:28:20 +0530

    sf

--- 27-02-23/variables.php
+++ 27-02-23/variables.php
@@ -17,9 +17,12 @@
 
     // echo "I have a ${color}ish dog named dog named $dog_name and her favorite food is {$favorite_food}";
 
-    $fairst_palyer_rank = "importennt";
-    $second_palyer_rank = $fairst_palyer_rank . "life";
+    // $fairst_palyer_rank = "importennt";
+    // $second_palyer_rank = $fairst_palyer_rank . "life";
 
-    echo $second_palyer_rank;
+    // echo $second_palyer_rank;
 
+    $full_name = "aisle";
+    $full_name = "nevertell";
+    echo $full_name;
 ?>
