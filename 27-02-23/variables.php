4d0e47a79d8d8913da4de13209bdad0e2450a195
gurukulsk <gurukulgetpass@gmail.com>
2023-02-27 08:10:14 +0530

    alalksdk

--- 27-02-23/variables.php
+++ 27-02-23/variables.php
@@ -3,8 +3,11 @@
     
     echo "<br>This is a best php file<br>";
     echo "<br>This is php  leactur in millenium hall<br>";
+    $favorite_food = "Hello";
+    echo "<br>$favorite_food<br>";
     
-    
+    $toy = "ramkadu";
+    echo "Smit is playing with $toy";
     echo  "</center>";
 
 ?>
