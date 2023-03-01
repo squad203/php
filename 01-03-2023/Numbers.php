594977123e718772b553512ca4f95231d9476a7b
squad203 <jakasaniyasmit@gmail.com>
2023-03-01 08:55:35 +0530

    cc

--- 01-03-2023/Numbers.php
+++ 01-03-2023/Numbers.php
@@ -1,31 +1,44 @@
 <?php
     
-    echo 5 +1;
-    echo "<br>";
-    echo  6.6  + 1.2;
-    echo "<br>";
-    echo 198263 -263;
-    echo "<br>";
-    echo -22.8 - 19.1;
-    echo "<br>";
-    echo "<br>";
+    // echo 5 +1;
+    // echo "<br>";
+    // echo  6.6  + 1.2;
+    // echo "<br>";
+    // echo 198263 -263;
+    // echo "<br>";
+    // echo -22.8 - 19.1;
+    // echo "<br>";
+    // echo "<br>";
     
     
-    $tadpole_age = 7 ;
-    $lily_age = 6 ;
-    $age_difference = $tadpole_age - $lily_age ;
-    echo $age_difference ;
+    // $tadpole_age = 7 ;
+    // $lily_age = 6 ;
+    // $age_difference = $tadpole_age - $lily_age ;
+    // echo $age_difference ;
     
-    echo "<br>";
-    $favorite_num = 22;
-    echo $favorite_num +1;
-    echo "<br>";
-    echo $favorite_num;
+    // echo "<br>";
+    // $favorite_num = 22;
+    // echo $favorite_num +1;
+    // echo "<br>";
+    // echo $favorite_num;
     
-    $num = 4 ;
-    $months = 11 ;
+    // $num = 4 ;
+    // $months = 11 ;
 
-    $days = 16 * $months;
-    echo $days;
-    $days_per_language  = $days
-?>
+    // $days = 16 * $months;
+    // echo $days;
+    // $days_per_language  = $days /$num;
+    // echo"<br>".$days_per_language;    
+    
+    // echo  3 ** 2;
+
+    $num_cookies = 27;
+    $cookies_per_serving = 4;
+    $leftover_cookies = $num_cookies % $cookies_per_serving;
+    echo $leftover_cookies;
+
+
+
+    
+    
+    ?>
