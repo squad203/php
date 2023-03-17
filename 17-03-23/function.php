e05253db0744efacd4350646668e004f8290431d
smartgurukul07 <smartgurukul07@outlook.com>
2023-03-17 08:32:05 +0530

    jm

--- 17-03-23/function.php
+++ 17-03-23/function.php
@@ -6,7 +6,11 @@ function sayCustomHello($name)
 function abc($str){
     echo $str."!";
 }
+function divide($num,$num2){
+    return $num / $num2
+}
+
 sayCustomHello("Aisle Nevertell");
 sayCustomHello("Codecadmy learner");
-
+divide(12,6)
 ?>
