0700e887f6ebe2e0669aaead93c52ba5a589ab9d
squad203 <jakasaniyasmit@gmail.com>
2023-03-17 08:04:21 +0530

    nndnddnnndndnnnnnnnnnnnnnnnnnnnnn

--- 17-03-23/function.php
+++ 17-03-23/function.php
@@ -1,12 +1,18 @@
 <?php
-function sayCustomHello($name)
-{
-    echo "hello,$name!";
-}
-function abc($str){
-    echo $str."!";
-}
-sayCustomHello("Aisle Nevertell");
-sayCustomHello("Codecadmy learner");
+// function sayCustomHello($name)
+// {
+//     echo "hello,$name!";
+// }
+// sayCustomHello("Aisle Nevertell");
+// sayCustomHello("Codecadmy learner");
+// function abc($str){
+//     echo $str."!";
+// }
 
-?>
+function acd($name)
+{
+    echo $name."#";
+    echo $name."#";
+    echo $name."#";
+}
+   acd("help")
